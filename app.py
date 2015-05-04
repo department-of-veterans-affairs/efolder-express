98dd0ae1ca4e6d7439ee9fd32ae61c616a253aa1
Alex Gaynor <alex.gaynor@gmail.com>
2015-05-04 15:06:47 -0400

    It seems to be working...

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import io
+import os
+import stat
+import subprocess
+import tempfile
 import zipfile
 
 import jinja2
@@ -23,15 +27,56 @@ class DownloadEFolder(object):
         t = self.jinja_env.get_template(template_name)
         return t.render({})
 
+    def _execute_connect_vbms(self, request, formatter):
+        ruby_code = """#!/usr/bin/env ruby
+
+require '/Users/alex_gaynor/projects/va/connect_vbms/src/vbms.rb'
+
+client = VBMS::Client.new(
+    "https://filenet.test.vbms.aide.oit.va.gov/vbmsp2-cms/streaming/eDocumentService-v4",
+    "/Users/alex_gaynor/projects/va/vbms-credentials/test/client3.jks",
+    "/Users/alex_gaynor/projects/va/vbms-credentials/test/samlToken-cui-tst.xml",
+    nil,
+    "importkey",
+    nil,
+    nil,
+)
+request = {request}
+result = client.send(request)
+STDOUT.write({formatter})
+        """.format(request=request, formatter=formatter).strip()
+        with tempfile.NamedTemporaryFile(suffix=".rb", delete=False) as f:
+            f.write(ruby_code)
+
+        st = os.stat(f.name)
+        os.chmod(f.name, st.st_mode | stat.S_IEXEC)
+
+        return subprocess.check_output([
+            "/Users/alex_gaynor/.gem/ruby/2.0.0/bin/bundle",
+            "exec",
+            f.name
+        ], cwd="/Users/alex_gaynor/projects/va/connect_vbms/")
+
     @app.route("/")
     def index(self, request):
         return self.render_template("index.html")
 
     @app.route("/download/", methods=["POST"])
     def download(self, request):
+        file_number = request.args["file_number"][0]
         data = io.BytesIO()
+
+        files = self._execute_connect_vbms(
+            "VBMS::Requests::ListDocuments.new({!r})".format(file_number),
+            'result.join("\\n")'
+        ).splitlines()
         with zipfile.ZipFile(data, "w") as zip_file:
-            pass
+            for i, f in enumerate(files):
+                contents = self._execute_connect_vbms(
+                    "VBMS::Requests::FetchDocumentById.new({!r})".format(f),
+                    "result",
+                )
+                zip_file.writestr("{}-eFolder/{}".format(file_number, i), contents)
         request.setHeader("Content-Type", "application/zip")
         return data.getvalue()
 
