2cae3fa9d3ce2842d2a5f6f06e9b832e0249d4c6
Alex Gaynor <alex.gaynor@gmail.com>
2015-05-06 11:03:32 -0400

    Move towards a more feedback driven UI

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import datetime
 import io
+import json
 import os
 import stat
 import sys
 import tempfile
+import uuid
 import zipfile
 
 import jinja2
@@ -11,10 +14,69 @@ import klein
 
 from pathlib import Path
 
-from twisted.internet.defer import Deferred, inlineCallbacks, returnValue
+from twisted.internet.defer import Deferred, inlineCallbacks, succeed
 from twisted.internet.task import react
 from twisted.internet.utils import getProcessOutput
+from twisted.python import log
 from twisted.web.server import Site
+
+
+class Document(object):
+    def __init__(self, document_id, doc_type, filename, received_at, source):
+        self.document_id = document_id
+        self.doc_type = doc_type
+        self.filename = filename
+        self.received_at = received_at
+        self.source = source
+
+        self.completed = False
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(
+            data["document_id"],
+            data["doc_type"],
+            data["filename"],
+            datetime.datetime.strptime(data["received_at"], "%Y-%m-%d").date(),
+            data["source"],
+        )
+
+
+class DownloadStatus(object):
+    def __init__(self, file_number, request_id):
+        self.file_number = file_number
+        self.request_id = request_id
+
+        self.has_manifest = False
+        self.manifest = []
+
+        self._io = io.BytesIO()
+        self.zipfile = zipfile.ZipFile(self._io, "w")
+
+    @property
+    def _completed_count(self):
+        return sum(1 for doc in self.manifest if doc.completed)
+
+    @property
+    def completed(self):
+        return self.manifest and self._completed_count == len(self.manifest)
+
+    @property
+    def percent_completed(self):
+        if not self.manifest:
+            return 5
+        return int(100 * (self._completed_count / float(len(self.manifest))))
+
+    def add_document(self, document):
+        self.has_manifest = True
+        self.manifest.append(document)
+
+    def add_document_contents(self, document, contents):
+        self.zipfile.writestr(
+            "{}-eFolder/{}".format(self.file_number, document.filename),
+            contents
+        )
+        document.completed = True
 
 
 class DownloadEFolder(object):
@@ -30,12 +92,16 @@ class DownloadEFolder(object):
             autoescape=True
         )
 
-    def render_template(self, template_name):
+        self.download_status = {}
+
+    def render_template(self, template_name, data={}):
         t = self.jinja_env.get_template(template_name)
-        return t.render({})
+        return t.render(data)
 
     def _execute_connect_vbms(self, request, formatter):
         ruby_code = """#!/usr/bin/env ruby
+
+require 'json'
 
 require '/Users/alex_gaynor/projects/va/connect_vbms/src/vbms.rb'
 
@@ -66,31 +132,60 @@ STDOUT.write({formatter})
             reactor=self.reactor
         )
 
+    @inlineCallbacks
+    def start_download(self, file_number, request_id):
+        status = DownloadStatus(file_number, request_id)
+        self.download_status[request_id] = status
+
+        documents = json.loads((yield self._execute_connect_vbms(
+            "VBMS::Requests::ListDocuments.new({!r})".format(file_number),
+            'result.map(&:to_h).to_json'
+        )))
+
+        for doc in documents:
+            document = Document.from_json(doc)
+            status.add_document(document)
+
+            self.start_file_download(status, document).addErrback(log.err)
+
+    @inlineCallbacks
+    def start_file_download(self, status, document):
+        contents = yield self._execute_connect_vbms(
+            "VBMS::Requests::FetchDocumentById.new({!r})".format(str(document.document_id)),
+            "result.content",
+        )
+        status.add_document_contents(document, contents)
+
     @app.route("/")
     def index(self, request):
         return self.render_template("index.html")
 
     @app.route("/download/", methods=["POST"])
-    @inlineCallbacks
     def download(self, request):
         file_number = request.args["file_number"][0]
-        data = io.BytesIO()
 
-        files = (yield self._execute_connect_vbms(
-            "VBMS::Requests::ListDocuments.new({!r})".format(file_number),
-            'result.map(&:document_id).join("\\n")'
-        )).splitlines()
-        with zipfile.ZipFile(data, "w") as zip_file:
-            for i, f in enumerate(files):
-                contents = yield self._execute_connect_vbms(
-                    "VBMS::Requests::FetchDocumentById.new({!r})".format(f),
-                    "result.content",
-                )
-                zip_file.writestr(
-                    "{}-eFolder/{}".format(file_number, i), contents
-                )
+        request_id = str(uuid.uuid4())
+
+        self.start_download(file_number, request_id).addErrback(log.err)
+
+        request.redirect("/download/{}/".format(request_id))
+        return succeed(None)
+
+    @app.route("/download/<request_id>/")
+    def download_status(self, request, request_id):
+        status = self.download_status[request_id]
+        return self.render_template("download.html", {"status": status})
+
+    @app.route("/download/<request_id>/zip/")
+    def download_zip(self, request, request_id):
+        status = self.download_status[request_id]
+        assert status.completed
+        status.zipfile.close()
+
         request.setHeader("Content-Type", "application/zip")
-        returnValue(data.getvalue())
+        contents = status._io.getvalue()
+        del self.download_status[request_id]
+        return contents
 
 
 def main(reactor):
