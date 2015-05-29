9fbbfc1d999c3949ef13c80ee018dafa416f2a41
Alex Gaynor <alex.gaynor@gmail.com>
2015-05-29 14:37:52 -0400

    Added tests for DownloadDatabase.create_documents

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,10 @@
+import datetime
+
 import pytest
 
 from sqlalchemy.schema import CreateTable
 
-from efolder_express.db import DownloadDatabase, DownloadNotFound
+from efolder_express.db import DownloadDatabase, DownloadNotFound, Document
 
 from .utils import FakeReactor, FakeThreadPool, success_result_of
 
@@ -40,6 +42,9 @@ class TestDownloadDatabase(object):
         assert download.state == "STARTED"
         assert download.documents == []
 
+        assert not download.completed
+        assert download.percent_completed == 5
+
     def test_get_download_non_existent(self, db):
         d = db.get_download("non-existent")
         with pytest.raises(DownloadNotFound):
@@ -54,3 +59,29 @@ class TestDownloadDatabase(object):
 
         download = success_result_of(db.get_download("test-request-id"))
         assert download.state == "ERRORED"
+
+    def test_create_documents(self, db):
+        d = db.create_download("test-request-id", "123456789")
+        success_result_of(d)
+
+        db.create_documents([
+            Document(
+                id="test-document-id",
+                download_id="test-request-id",
+                document_id="{ABCD}",
+                doc_type="00356",
+                filename="file.pdf",
+                received_at=datetime.datetime.utcnow(),
+                source="CUI",
+                content_location=None,
+                errored=False,
+            )
+        ])
+
+        download = success_result_of(db.get_download("test-request-id"))
+        assert len(download.documents) == 1
+        assert not download.completed
+        assert download.percent_completed == 0
+
+        [doc] = download.documents
+        assert doc.id == "test-document-id"
